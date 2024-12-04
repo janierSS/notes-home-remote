262ee42c5c112f87cea474402cd40cf800a30733
Janier <janierss17@gmail.com>
2024-12-04 17:07:46 -0500

    add auth receipt provider for development

--- src/mainDev.tsx
+++ src/mainDev.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import appStore  from "myNotesHost/appStore";
+import { Provider } from "react-redux";
+import appStore from "myNotesHost/appStore";
+import AuthReceiptProvider from "myNotesHost/AuthReceiptProvider";
 import AppDev from "./AppDev.tsx";
 import "./mainDev.scss";
-import { Provider } from "react-redux";
-
-console.log('appaStore: ', appStore);
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={appStore}>
-      <AppDev />
+      <AuthReceiptProvider>
+        <AppDev />
+      </AuthReceiptProvider>
     </Provider>
   </StrictMode>
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite/client" />
 
 declare module "myNotesHost/appStore"
-
 declare module "myNotesHost/selectAuthReceipt"
+declare module "myNotesHost/AuthReceiptProvider" {
+    const AuthReceiptProvider: React.ComponentType<{children: React.ReactNode}>;
+    export default AuthReceiptProvider;
+  }
