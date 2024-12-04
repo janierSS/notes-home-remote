6c3af3e4612ec61af23926697602d43f32f77f37
Janier <janierss17@gmail.com>
2024-12-04 15:32:51 -0500

    Add store for dev

--- src/mainDev.tsx
+++ src/mainDev.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import appStore  from "myNotesHost/appStore";
+import appStore  from "myNotesHost/appStore";
 import AppDev from "./AppDev.tsx";
 import "./mainDev.scss";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
-// console.log('appaStore: ', appStore);
+console.log('appaStore: ', appStore);
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <Provider store={appStore}> */}
+    <Provider store={appStore}>
       <AppDev />
-    {/* </Provider> */}
+    </Provider>
   </StrictMode>
 );
